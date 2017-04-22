822854fde3060dc6cb551885ecf3cd145f380aa1
Eduardo <xJupiter931>
2017-04-22 11:11:51 +0100

    Added enemy logic and direction control

---
+++ PortoGameJam2017/Assets/Scripts/EnemyController2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController2D : MonoBehaviour
+{
+	private Transform player_transform;
+	private bool in_range;
+
+	public float engageRange;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player_transform = FindObjectOfType<PlayerController2D> ().transform;
+
+		if (player_transform.Equals(null))
+			Debug.Log ("Couldn't find player transform");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	// Fixed Update
+	void FixedUpdate ()
+	{
+		in_range = checkInRange ();
+
+		if (in_range)
+			Debug.Log ("In Range");
+		else
+			Debug.Log ("Not In Range");
+	}
+
+	// Checks wether the player is in engage range or not
+	bool checkInRange()
+	{
+		float distance = Vector3.Distance (transform.position, player_transform.position);
+		return (distance <= engageRange);
+	}
+}
--- PortoGameJam2017/Assets/Scripts/PlayerController2D.cs
+++ PortoGameJam2017/Assets/Scripts/PlayerController2D.cs
@@ -8,11 +8,14 @@ public class PlayerController2D : MonoBehaviour
 	private float x_axis;
 	private float y_axis;
 
+	private int dir; // default 0
+
 	public float velocity;
 
 	// Use this for initialization
 	void Start ()
 	{
+		dir = 0;
 		Rb2D = GetComponent<Rigidbody2D> ();
 	}
 	
@@ -23,5 +26,56 @@ public class PlayerController2D : MonoBehaviour
 		y_axis = Input.GetAxisRaw ("Vertical");
 
 		Rb2D.velocity = new Vector2 (x_axis, y_axis) * velocity;
+
+		updateDir ();
+	}
+
+	// Return the direction (1 direita, 2 esquerda, 3 cima, 4 baixo)
+	void updateDir()
+	{
+		if (x_axis == 1)
+		{
+			dir = 1;
+			return;
+		}
+
+		if (x_axis == -1)
+		{
+			dir = 2;
+			return;
+		}
+
+		if (y_axis == 1)
+		{
+			dir = 3;
+			return;
+		}
+
+
+		if (y_axis == -1)
+		{
+			dir = 4;
+			return;
+		}
+	}
+
+	// Parses dir
+	string getDirAsString()
+	{
+		switch (dir)
+		{
+		case 0:
+			return "Parado";
+		case 1:
+			return "Direita";
+		case 2:
+			return "Esquerda";
+		case 3:
+			return "Cima";
+		case 4:
+			return "Baixo";
+		default:
+			return "Error Parsing Direction";
+		}
 	}
 }
