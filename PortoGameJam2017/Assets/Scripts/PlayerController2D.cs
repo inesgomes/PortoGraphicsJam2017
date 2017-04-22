57562adc92a0c9b842a280b4aa2147f9364a416f
Eduardo <xJupiter931>
2017-04-22 10:37:34 +0100

    2D basic physics done

---
+++ PortoGameJam2017/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+	private Rigidbody2D Rb2D;
+	private float x_axis;
+	private float y_axis;
+
+	public float velocity;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Rb2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		x_axis = Input.GetAxisRaw ("Horizontal");
+		y_axis = Input.GetAxisRaw ("Vertical");
+
+		Rb2D.velocity = new Vector2 (x_axis, y_axis) * velocity;
+	}
+}
