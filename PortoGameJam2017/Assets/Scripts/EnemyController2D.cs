64d6286ea98f87ab588913b4c1ada78093dcd4ba
Eduardo <xJupiter931>
2017-04-22 12:08:55 +0100

    Enemy movement done :D

--- PortoGameJam2017/Assets/Scripts/EnemyController2D.cs
+++ PortoGameJam2017/Assets/Scripts/EnemyController2D.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyController2D : MonoBehaviour
 {
+	private Rigidbody2D Rb2D;
 	private Transform player_transform;
 	private bool in_range;
 
 	public float engageRange;
+	public float velocity;
+	public float minDist;
 
 	// Use this for initialization
 	void Start ()
 	{
 		player_transform = FindObjectOfType<PlayerController2D> ().transform;
+		Rb2D = GetComponent<Rigidbody2D> ();
 
 		if (player_transform.Equals(null))
 			Debug.Log ("Couldn't find player transform");
@@ -21,24 +25,15 @@ public class EnemyController2D : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		
-	}
-
-	// Fixed Update
-	void FixedUpdate ()
-	{
-		in_range = checkInRange ();
-
-		if (in_range)
-			Debug.Log ("In Range");
-		else
-			Debug.Log ("Not In Range");
-	}
-
-	// Checks wether the player is in engage range or not
-	bool checkInRange()
-	{
 		float distance = Vector3.Distance (transform.position, player_transform.position);
-		return (distance <= engageRange);
+
+		if (distance <= engageRange && distance > minDist)
+		{
+			Vector2 vector_dir = (player_transform.position - transform.position) / distance;
+			Rb2D.velocity = (vector_dir * velocity);
+		}
+		else
+			Rb2D.velocity = Vector2.zero;
 	}
+
 }
